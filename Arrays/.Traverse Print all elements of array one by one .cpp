fc852e07f06696ab6553a3aa427c2d3fd0304742
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-08-13 14:53:32 +0530

    Create .Traverse Print all elements of array one by one .cpp

---
+++ Arrays/.Traverse Print all elements of array one by one .cpp
@@ -0,0 +1,11 @@
+//Traverse Print all elements of array one by one .
+
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[5]={32,16,71,10,12};
+	//Traverse the Array
+	for(int i=0;i<=4;i++)
+	cout<<arr[i]<<endl;
+	return 0;
+}
