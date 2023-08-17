11f638ba5d5c91f7888b2b998d327aaa5c240ebf
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-08-17 19:42:35 +0530

    Create Circular Linked List.cpp

---
+++ Linked Lists/Circular Linked List.cpp
@@ -0,0 +1,40 @@
+//Circular Linked List
+#include<iostream>
+using namespace std;
+struct Node{
+	int data;
+	Node *link;
+}*start;
+int main(){
+	Node N1,N2,N3,N4,N5;
+	N1.data=27;
+	N2.data=36;
+	N3.data=41;
+	N4.data=59;
+	N5.data=74;
+	N1.link=&N2;
+	N2.link=&N3;
+	N3.link=&N4;
+	N4.link=&N5;
+	N5.link=NULL;
+	start=&N1;
+	while(start!=NULL){
+		cout<<start->data<<endl;
+		start=start->link;
+	}
+	cout<<"Circular Linked List"<<endl;
+		
+	N1.link=&N2;
+	start=&N1;
+	N2.link=&N3;
+	N3.link=&N4;
+	N4.link=&N5;
+	N5.link=start;
+
+
+	while(start!=NULL){
+		cout<<start->data<<endl;
+		start=start->link;
+	}
+	return 0;
+}
